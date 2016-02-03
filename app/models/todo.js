3f9df497725b0daa11226fdad6dc36801a7143f3
SuperManEver <nikita.luparev@gmail.com>
2016-02-03 17:37:40 +0500

    added fixture adapter and components for todo-item and todo-list

---
+++ app/models/todo.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+
+var Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  complete: DS.attr('boolean')
+});
+
+Todo.reopenClass({
+  FIXTURES: [
+    {id: 1, title: 'Buy Eggs', complete: false},
+    {id: 2, title: 'Buy Milk',complete: false },
+    {id: 3, title: "Become member of the core team", complete: false},
+    {id: 4, title: "become millionare", complete: false}
+  ]
+});
+
+export default Todo;
